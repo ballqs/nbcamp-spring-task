638feff7a48a85ac19749b8d486bd6fc783b3e83
ballqs <46590521+ballqs@users.noreply.github.com>
2024-08-11 08:31:40 +0900

    spring git commit

---
+++ src/main/java/com/sparta/nbcampspringtask/NbcampSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampspringtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampSpringTaskApplication.class, args);
+    }
+
+}
