915e2ca58e13afa8adc992d552970646db534f26
ballqs <46590521+ballqs@users.noreply.github.com>
2024-08-18 17:11:50 +0900

    스프링 입문 개인과제 피드백 3차 적용 .env 파일을 활용하여 민감한 데이터 숨김처리

--- src/main/java/com/sparta/nbcampspringtask/NbcampSpringTaskApplication.java
+++ src/main/java/com/sparta/nbcampspringtask/NbcampSpringTaskApplication.java
@@ -1,5 +1,6 @@
 package com.sparta.nbcampspringtask;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NbcampSpringTaskApplication {
 
     public static void main(String[] args) {
+        // .env 파일 로드
+        // 다른 위치에 있다면 Dotenv.configure().directory("/your/path").load()를 사용하여 경로를 지정
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // 환경 변수를 시스템 속성으로 설정
+        System.setProperty("DB_IP", dotenv.get("DB_IP"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NM", dotenv.get("DB_NM"));
+        System.setProperty("DB_ID", dotenv.get("DB_ID"));
+        System.setProperty("DB_PW", dotenv.get("DB_PW"));
+
         SpringApplication.run(NbcampSpringTaskApplication.class, args);
     }
 
